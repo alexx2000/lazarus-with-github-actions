3bc0fe90b31de9f46dabf92e6ce2927794d68cfe
Alexander Koblov <alexx2000@mail.ru>
2022-10-25 20:38:47 +0300

    Update upload.sh

--- .github/scripts/upload.sh
+++ .github/scripts/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "$SSH_PRIVATE_KEY" > ssh_key && chmod 0600 ssh_key
-sftp -o StrictHostKeyChecking=no -i ssh_key -b ./.github/upload.txt $REMOTE_USER@$REMOTE_HOST
+sftp -o StrictHostKeyChecking=no -i ssh_key -b ./.github/scripts/upload.txt $REMOTE_USER@$REMOTE_HOST
 
 
