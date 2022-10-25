bfa9949bf5e57e9ea37046555e7cb852d7f43634
Alexander Koblov <alexx2000@mail.ru>
2022-10-25 21:50:39 +0300

    Update upload.sh

--- .github/scripts/upload.sh
+++ .github/scripts/upload.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "$SSH_PRIVATE_KEY" > ssh_key && chmod 0600 ssh_key
-sftp -o StrictHostKeyChecking=no -i ssh_key -b ./.github/scripts/upload.txt $REMOTE_USER@$REMOTE_HOST
-
-
+sftp -o StrictHostKeyChecking=no -i ssh_key $REMOTE_USER@$REMOTE_HOST <<END
+cd /home/project-web/doublecmd/htdocs/snapshots
+put src/resourceunit.pas
+quit
+END
