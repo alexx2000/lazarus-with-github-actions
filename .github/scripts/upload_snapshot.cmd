75fd7aa2726432d823d15f0c9f9de46e85a5cd6d
Alexander Koblov <alexx2000@mail.ru>
2023-04-13 22:13:29 +0300

    UPD: Scripts

---
+++ .github/scripts/upload_snapshot.cmd
@@ -0,0 +1,15 @@
+@ECHO OFF
+echo "$SSH_PRIVATE_KEY" > ssh_key
+
+icacls.exe ssh_key //reset
+icacls.exe ssh_key //grant:r "$USERNAME:(R)"
+icacls.exe ssh_key //inheritance:r
+
+sftp -o StrictHostKeyChecking=no -i ssh_key $REMOTE_USER@$REMOTE_HOST <<END
+cd /home/project-web/doublecmd/htdocs/snapshots
+lcd /var/tmp/doublecmd-release
+rm *.zip
+put *.zip
+put *.txt
+quit
+END
