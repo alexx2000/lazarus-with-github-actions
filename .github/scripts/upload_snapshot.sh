701699f38473b0c99dba5cf707fe202a24bba61c
Alexander Koblov <alexx2000@mail.ru>
2023-04-13 22:53:15 +0300

    UPD: Scripts

--- .github/scripts/upload_snapshot.sh
+++ .github/scripts/upload_snapshot.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 @ECHO OFF
+
+echo $OSTYPE
+
 echo "$SSH_PRIVATE_KEY" > ssh_key
 
-icacls.exe ssh_key /reset
-icacls.exe ssh_key /inheritance:r
-icacls.exe ssh_key
+#icacls.exe ssh_key //reset
+icacls.exe ssh_key //inheritance:r
 
-echo "cd /home/project-web/doublecmd/htdocs/snapshots" > upload_snapshot.txt
-echo "lcd /var/tmp/doublecmd-release" >> upload_snapshot.txt
-echo "rm *.zip" >> upload_snapshot.txt
-echo "put *.zip" >> upload_snapshot.txt
-echo "put *.txt" >> upload_snapshot.txt
-echo "quit" >> upload_snapshot.txt
-
-sftp -o StrictHostKeyChecking=no -i ssh_key -b upload_snapshot.txt $REMOTE_USER@$REMOTE_HOST
+sftp -o StrictHostKeyChecking=no -i ssh_key $REMOTE_USER@$REMOTE_HOST <<END
+cd /home/project-web/doublecmd/htdocs/snapshots
+lcd /var/tmp/doublecmd-release
+rm *.zip
+put *.zip
+put *.txt
+quit
+END
