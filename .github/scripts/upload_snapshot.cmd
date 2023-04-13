eefd4b6b0425034910b56c4bd726cfbe12c57a47
Alexander Koblov <alexx2000@mail.ru>
2023-04-13 22:38:40 +0300

    UPD: Scripts

--- .github/scripts/upload_snapshot.cmd
+++ .github/scripts/upload_snapshot.cmd
@@ -1,15 +1,15 @@
-@echo off
-@echo %SSH_PRIVATE_KEY%> ssh_key
+@ECHO OFF
+echo "$SSH_PRIVATE_KEY" > ssh_key
 
 icacls.exe ssh_key /reset
-rem icacls.exe ssh_key /grant:r "$USERNAME:(R)"
+#icacls.exe ssh_key /grant:r "$USERNAME:(R)"
 icacls.exe ssh_key /inheritance:r
 
-echo cd /home/project-web/doublecmd/htdocs/snapshots> upload_snapshot.txt
-echo lcd release>> upload_snapshot.txt
-echo rm *.zip>> upload_snapshot.txt
-echo put *.zip>> upload_snapshot.txt
-echo put *.txt>> upload_snapshot.txt
-echo quit>> upload_snapshot.txt
+echo "cd /home/project-web/doublecmd/htdocs/snapshots" > upload_snapshot.txt
+echo "lcd /var/tmp/doublecmd-release" >> upload_snapshot.txt
+echo "rm *.zip" >> upload_snapshot.txt
+echo "put *.zip" >> upload_snapshot.txt
+echo "put *.txt" >> upload_snapshot.txt
+echo "quit" >> upload_snapshot.txt
 
-sftp -o StrictHostKeyChecking=no -i ssh_key -b upload_snapshot.txt %REMOTE_USER%@%REMOTE_HOST%
+sftp -o StrictHostKeyChecking=no -i ssh_key -b upload_snapshot.txt $REMOTE_USER@$REMOTE_HOST
