fb574a4d4569517e7948c3cf864b56d3142cf5e7
Alexander Koblov <alexx2000@mail.ru>
2023-04-13 22:21:07 +0300

    Update upload_snapshot.cmd

--- .github/scripts/upload_snapshot.cmd
+++ .github/scripts/upload_snapshot.cmd
@@ -2,7 +2,7 @@
 echo "$SSH_PRIVATE_KEY" > ssh_key
 
 icacls.exe ssh_key /reset
-icacls.exe ssh_key /grant:r "$USERNAME:(R)"
+#icacls.exe ssh_key /grant:r "$USERNAME:(R)"
 icacls.exe ssh_key /inheritance:r
 
 sftp -o StrictHostKeyChecking=no -i ssh_key $REMOTE_USER@$REMOTE_HOST <<END
